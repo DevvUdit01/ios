74ef268153f7cdf96f619207cca0f322eba99953
DevvUdit01 <devv.udit@gmail.com>
2026-06-24 15:42:40 +0530

    version upgrade issue solved

--- Project.swift
+++ Project.swift
@@ -10,7 +10,7 @@ let projectName = Environment.projectName.getString(default: "MedianIOS")
 let targetName = Environment.targetName.getString(default: "Median")
 let bundleId = "com.thejobsadvertise.app"
 let marketingVersion = Environment.marketingVersion.getString(default: "1.0")
-let projectVersion = Environment.projectVersion.getString(default: "3")
+let projectVersion = Environment.projectVersion.getString(default: "4")
 let appName = "The Jobs Advertise"
 
 let project = Project(
